d201662eb72549aa5e8d5f380e91ce1bf67c0bd9
AnarchoMax <72539377+Eetchibauls@users.noreply.github.com>
2020-10-15 13:58:29 -0700

    Revert "Update letsburn.js"
    This reverts commit c7cb4929f5d2a2446a156a538b1ede1d01bbe77d.

--- letsburn.js
+++ letsburn.js
@@ -226,6 +226,20 @@ const getYFKASupply = async () => {
 
 }
 
+
+$('input[type=radio][name=stake]').change(async (event) => {
+  console.log('change radio stake');
+  const balances = await getPoolBalances();
+  console.log('balances: ', balances);
+  const balance = balances[event.currentTarget.value];
+  console.log('balance: ', balance);
+  // TODO
+  $('#stake-input').val(`${balance}`);
+  $('#stake-input').attr('placeholder', `${balance}`);
+  $('#stake-balance').html(`${balance}`)
+  return balance || '';
+});
+
 function waitForApproval(tx, contractInstance, payload) {
  	web3.eth.getTransaction(tx,
  		function (err, res2) {
@@ -247,20 +261,6 @@ function waitForApproval(tx, contractInstance, payload) {
 
 
 
-
-$('input[type=radio][name=stake]').change(async (event) => {
-  console.log('change radio stake');
-  const balances = await getPoolBalances();
-  console.log('balances: ', balances);
-  const balance = balances[event.currentTarget.value];
-  console.log('balance: ', balance);
-  // TODO
-  $('#stake-input').val(`${balance}`);
-  $('#stake-input').attr('placeholder', `${balance}`);
-  $('#stake-balance').html(`${balance}`)
-  return balance || '';
-});
-
 $('#stakeBTN').click(async () => {
 	
 var uniContract = web3.eth.contract(uniTokenABI);
@@ -268,9 +268,6 @@ var uniContract = web3.eth.contract(uniTokenABI);
   const keys = Object.keys(PAIRS);
   console.log('keys: ', keys);
   const value = $('[name=stake][type=radio]:checked').val();
-  var ashContract = web3.eth.contract(uniTokenABI);
-  var contractInstance = ashContract.at(YFKA_CONTROLLER_ADDRESS);
-  var payload;
 //PULL uniInstance info from radio button.
   switch (value){
 	case 'XAMP':
@@ -633,6 +630,10 @@ window.addEventListener('load', async (event) => {
     $('#stake-balance').html(`${balance}`);
     console.log(poolBalances);
 });
+
+
+
+
 
 
 
