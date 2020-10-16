c4e2b448fcd25dd2e624d3e0667659daee03d3af
Chris Livermore <chris.livermore@hotmail.com>
2020-10-16 23:20:08 +0100

    Update letsburn.js

--- letsburn.js
+++ letsburn.js
@@ -277,22 +277,22 @@ const updateUserStats = async () => {
 	});
 	//% of pool
 	var TotalXAMPbalance = 0;
-	contractInstance.totalYFKAStaked(0, function (err, res) {
+	await contractInstance.totalYFKAStaked(0, function (err, res) {
 		TotalXAMPbalance = (res / (10 ** 18));
 		console.log('TOTAL XAMP: ', TotalXAMPbalance);
 	});
 	var TotalTOBbalance = 0;
-	contractInstance.totalYFKAStaked(1, function (err, res) {
+	await contractInstance.totalYFKAStaked(1, function (err, res) {
 		TotalTOBbalance = (res / (10 ** 18));
 		console.log('TOTAL TOB: ', TotalTOBbalance);
 	});
 	var TotalBOAbalance = 0;
-	contractInstance.totalYFKAStaked(2, function (err, res) {
+	await contractInstance.totalYFKAStaked(2, function (err, res) {
 		TotalBOAbalance = (res / (10 ** 18));
 		console.log('TOTAL BOA: ', TotalBOAbalance);
 	});
 	var TotalETHbalance = 0;
-	contractInstance.totalYFKAStaked(3, function (err, res) {
+	await contractInstance.totalYFKAStaked(3, function (err, res) {
 		TotalETHbalance = (res / (10 ** 18));
 		console.log('TOTAL ETH: ', TotalETHbalance);
 	});
