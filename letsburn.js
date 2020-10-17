bd10c8a6c822adbb76d6a15874fc61bd462dab07
Chris Livermore <chris.livermore@hotmail.com>
2020-10-17 11:07:46 +0100

    Update letsburn.js

--- letsburn.js
+++ letsburn.js
@@ -158,7 +158,7 @@ function twoDecimals(number){
 }
 function fourDecimals(number){
 	//returns the input with 4 Decimal places. ALWAYS WORKS OUT FLOOR
-	if number > 1{
+	if (number > 1){
 	const newNumber = Math.floor((number+ Number.EPSILON) * 10000)/100;
 	}else
 	{
