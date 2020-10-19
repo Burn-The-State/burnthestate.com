757b1fcac3a243eda14b1950139ffdcfb6312706
Chris Livermore <chris.livermore@hotmail.com>
2020-10-19 10:56:29 +0100

    Decimal Testing
    - Added Logging for Decimal testing.

--- letsburn.js
+++ letsburn.js
@@ -902,11 +902,16 @@ const getIndexBySymbol = (value) => {
 };
 
 function twoDecimals(b) {
+	console.log('Two Decimals Recieved( ', b, ')');
 	const balance = Number(b).toLocaleString('fullwide', {useGrouping:false});
 	// TODO add this back + test
+	console.log('Balance after to String( ', balance, ')');
+	const numericalbalance = _.toNumber(balance);
+	console.log('Numerical Balance( ', numericalbalance, ')');
   //returns the input with 2 Decimal places. ALWAYS WORKS OUT FLOOR
-  const newNumber = Math.floor((balance + Number.EPSILON) * 100) / 100;
-  return _.toNumber(newNumber);
+  const newNumber = Math.floor((numericalbalance + Number.EPSILON) * 100) / 100;
+  console.log('Passing ( ', newNumber, ')');
+  return newNumber;
 }
 
 function fourDecimals(b) {
