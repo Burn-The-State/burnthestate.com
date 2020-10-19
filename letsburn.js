855ab05a645ca28b7cce9aa74c42c70a3a5dd178
Chris Livermore <chris.livermore@hotmail.com>
2020-10-19 20:12:15 +0100

    Update letsburn.js

--- letsburn.js
+++ letsburn.js
@@ -843,8 +843,16 @@ const YFKA_CONTROLLER_ABI = [
 *
 *
 */
+
+const Web3 = require("web3");
+
 const isConnected = () => {
-  return !!web3.isConnected();
+   if (window.ethereum) {
+    window.web3 = new Web3(window.ethereum);
+    window.ethereum.enable();
+    return true;
+  }
+  return false;
 };
 
 const getInfuraProvider = () => {
