f3d6659719d5613cf5fdf6b3abf6b03820057976
Chris Livermore <chris.livermore@hotmail.com>
2020-10-22 00:52:55 +0100

    Update letsburn.js

--- letsburn.js
+++ letsburn.js
@@ -1649,9 +1649,9 @@ window.addEventListener('load', async (event) => {
 			console.log('METAMASK NOT CONNECTED!');
 			//updatePoolBalances();
 			$('#isConnected').html('wallet connected');
-			await updateActivePool().catch(e);
+			await updateActivePool().catch(e = e.message);
 			if (e != null && DISPLAY_ERRORS) console.log('ERROR (updateActivePool): ',e);
-			await updateUserStats().catch(e));
+			await updateUserStats().catch(e = e.message);
 			if (e != null && DISPLAY_ERRORS) console.log('ERROR (updateUserStats): ',e);
 		await setStakeBalance({
 			currentTarget: {
