5fdacc3ecc0d3bceeb18a798de4410a2f68209c6
zer0cache <ssfaleads@gmail.com>
2020-10-23 21:17:28 -0400

    .

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: './letsburn.js',
+    output: {
+        path: __dirname,
+        filename: 'index.js'
+    },
+    module: {
+        rules: [{
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }]
+    }
+}
