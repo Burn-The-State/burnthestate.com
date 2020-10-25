33ee7d6c0dc8ac9642ceb48ededb7a3ed43809d6
Chris Livermore <chris.livermore@hotmail.com>
2020-10-25 23:35:44 +0000

    Update letsburn.js

--- letsburn.js
+++ letsburn.js
@@ -1302,7 +1302,7 @@ const FillInfo = async () => {
 	$('#BOALP').html(Number(twoDecimals(BOAtoLP)).toLocaleString());
 	$('#ETHLP').html(Number(twoDecimals(ETHtoLP)).toLocaleString());
 	
-	$('#LPXAMP').html(tenDecimals(toFixed(LPtoXAMP-(LPtoXAMP*feeCalc))));
+	$('#LPXAMP').html(toFixed(tenDecimals(LPtoXAMP-(LPtoXAMP*feeCalc))));
 	$('#LPTOB').html(eightDecimals((LPtoTOB-(LPtoTOB*feeCalc))));
 	$('#LPBOA').html(eightDecimals((LPtoBOA-(LPtoBOA*feeCalc))));
 	$('#LPETH').html(eightDecimals((LPtoETH-(LPtoETH*feeCalc))));
