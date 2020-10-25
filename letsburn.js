5ec160db79d80856feef01ddc96138665f2cd0b1
Chris Livermore <chris.livermore@hotmail.com>
2020-10-25 23:31:53 +0000

    Update letsburn.js

--- letsburn.js
+++ letsburn.js
@@ -1302,7 +1302,7 @@ const FillInfo = async () => {
 	$('#BOALP').html(Number(twoDecimals(BOAtoLP)).toLocaleString());
 	$('#ETHLP').html(Number(twoDecimals(ETHtoLP)).toLocaleString());
 	
-	$('#LPXAMP').html(tenDecimals(LPtoXAMP-(LPtoXAMP*feeCalc)));
+	$('#LPXAMP').html(toFixed(LPtoXAMP-(LPtoXAMP*feeCalc)));
 	$('#LPTOB').html(eightDecimals((LPtoTOB-(LPtoTOB*feeCalc))));
 	$('#LPBOA').html(eightDecimals((LPtoBOA-(LPtoBOA*feeCalc))));
 	$('#LPETH').html(eightDecimals((LPtoETH-(LPtoETH*feeCalc))));
@@ -1391,7 +1391,23 @@ const FillInfo = async () => {
 };
 
 
-
+function toFixed(x) {
+  if (Math.abs(x) < 1.0) {
+    var e = parseInt(x.toString().split('e-')[1]);
+    if (e) {
+        x *= Math.pow(10,e-1);
+        x = '0.' + (new Array(e)).join('0') + x.toString().substring(2);
+    }
+  } else {
+    var e = parseInt(x.toString().split('+')[1]);
+    if (e > 20) {
+        e -= 20;
+        x /= Math.pow(10,e);
+        x += (new Array(e+1)).join('0');
+    }
+  }
+  return x;
+}
 
 
 
