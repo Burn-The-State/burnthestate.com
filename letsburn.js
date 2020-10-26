42269ff5d3625e66a660b01873b355c47c4049a8
Chris Livermore <chris.livermore@hotmail.com>
2020-10-26 03:38:01 +0000

    Update letsburn.js

--- letsburn.js
+++ letsburn.js
@@ -1345,7 +1345,7 @@ const FillInfo = async () => {
 	//WALLET DUMP
 	//XAMP
 	$('#TOTXAMP').html(BTSTOT.fXAMPTotal);
-	$('#LPXAMPuser').html(BTSTOT.fXAMPLP /(10**18));
+	$('#LPXAMPuser').html(BTSTOT.fXAMPLP /(10**9));
 	$('#WALXAMP').html(BTSTOT.fXAMPWallet);
 	//XAMP $
 	$('#XAMPP1').html(twoDecimals(XAMPPrice.usd*(BTSTOT.fXAMPTotal)));
