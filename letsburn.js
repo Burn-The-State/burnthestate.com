adeecbb0603cd68f560916e1f0851d7be564bf87
Chris Livermore <chris.livermore@hotmail.com>
2020-10-27 15:53:43 +0000

    Update letsburn.js
    - Added YFKA per LP to pool info

--- letsburn.js
+++ letsburn.js
@@ -1415,6 +1415,11 @@ const FillInfo = async () => {
 	$('#BOALP').html(Number(fourDecimals(BOAtoLP)).toLocaleString());
 	$('#ETHLP').html(Number(fourDecimals(ETHtoLP)).toLocaleString());
 	
+	$('#YFKALP').html(Number(fourDecimals(YFKAtotX)).toLocaleString());
+	$('#YFKALP').html(Number(fourDecimals(YFKAtotT)).toLocaleString());
+	$('#YFKALP').html(Number(fourDecimals(YFKAtotB)).toLocaleString());
+	$('#YFKALP').html(Number(fourDecimals(YFKAtotE)).toLocaleString());
+	
 	$('#LPXAMP').html(toFixed(tenDecimals(LPtoXAMP)));
 	$('#LPTOB').html(eightDecimals(sixDecimals(LPtoTOB)));
 	$('#LPBOA').html(eightDecimals(sixDecimals(LPtoBOA)));
