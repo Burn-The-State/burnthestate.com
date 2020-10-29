47d09ca76d1a45821f60ff0677d804c6ca818f4c
Chris Livermore <chris.livermore@hotmail.com>
2020-10-29 12:31:29 +0000

    Update letsburn.js

--- letsburn.js
+++ letsburn.js
@@ -1145,11 +1145,6 @@ const getLPconversions = async () =>
 
 
 
-	const XAMPfromLP = XAMPtoLP*(UsersLP.XAMP/(10**18));	
-	const TOBfromLP = TOBtoLP*(UsersLP.TOB/(10**18));
-	const BOAfromLP = BOAtoLP*(UsersLP.BOA/(10**18));
-	const ETHfromLP = ETHtoLP*(UsersLP.ETH/(10**18));
-	
 	if (DISPLAY_CONSOLE) console.log("YFKA LP XAMP : ",YFKAtoLPX );
 	if (DISPLAY_CONSOLE) console.log("YFKA LP TOB: ",YFKAtoLPT );
 	if (DISPLAY_CONSOLE) console.log("YFKA LP BOA: ",YFKAtoLPB );
@@ -1179,6 +1174,13 @@ const getBTSTotals = async () => {
 	const BTStoLP = await getLPconversions();
 	
 	
+	
+	
+	const XAMPfromLP = BTStoLP.XAMPtoLP*(UsersLP.XAMP/(10**18));	
+	const TOBfromLP = BTStoLP.TOBtoLP*(UsersLP.TOB/(10**18));
+	const BOAfromLP = BTStoLP.BOAtoLP*(UsersLP.BOA/(10**18));
+	const ETHfromLP = BTStoLP.ETHtoLP*(UsersLP.ETH/(10**18));
+		
 	const YFKAfromLP = (BTStoLP.YFKAtoLPXAMP*(UsersLP.XAMP/(10**18))) +
 						(BTStoLP.YFKAtoLPTOP *(UsersLP.TOB/(10**18)))+
 						(BTStoLP.YFKAtoLPBOA*(UsersLP.BOA/(10**18))) +
