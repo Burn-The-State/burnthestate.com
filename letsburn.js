e86c3eb5a0987f5b9637603e8caa6235de07608c
Chris Livermore <chris.livermore@hotmail.com>
2020-11-02 06:05:18 +0000

    Update letsburn.js

--- letsburn.js
+++ letsburn.js
@@ -2682,6 +2682,34 @@ const fillYFKAinfo = async () =>{
 	$('#PEthPooledYFKA').html(twoDecimals((await totalEthPool/await PooledYFKA)*100));
 	$('#EthStakedYFKA').html(twoDecimals(await stakedETH));
 	$('#PEthStakedYFKA').html(twoDecimals((await stakedETH/await totalYFKAStakerd)*100));
+	
+	
+	// Load google charts
+	google.charts.load('current', {'packages':['corechart']});
+	google.charts.setOnLoadCallback(drawChart);
+
+	// Draw the chart and set the chart values
+	function drawChart() {
+	  var data = google.visualization.arrayToDataTable([
+	  ['Task', 'Hours per Day'],
+	  ['Work', 8],
+	  ['Eat', 2],
+	  ['TV', 4],
+	  ['Gym', 2],
+	  ['Sleep', 8]
+	]);
+
+	  // Optional; add a title and set the width and height of the chart
+	  var options = {'title':'My Average Day', 'width':550, 'height':400};
+
+	  // Display the chart inside the <div> element with id="piechart"
+	  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+	  chart.draw(data, options);
+}
+	
+	
+	
+	
 }
 
 /*
@@ -3110,5 +3138,6 @@ window.addEventListener('load', async (event) => {
 	});
 	
 });
+
 
 
